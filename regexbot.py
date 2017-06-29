959835bffc66f6b9b5d4e7ad163fda97e91908be
Sijmen Schoon <me@sijmenschoon.nl>
2017-06-29 23:24:15 +0200

    Fix reply regex to images, and tiny refactor

--- regexbot.py
+++ regexbot.py
@@ -10,6 +10,15 @@ bot = Bot(api_token=os.environ['API_KEY'])
 
 last_msgs = defaultdict(list)
 
+
+def find_original(message):
+    if 'text' in message:
+        return message['text']
+    elif 'caption' in message:
+        return message['caption']
+
+    return None
+
 async def doit(chat, match):
     fr = match.group(1)
     to = match.group(2)
@@ -22,6 +31,7 @@ async def doit(chat, match):
     except IndexError:
         fl = ''
 
+    # Build Python regex flags
     count = 1
     flags = 0
     for f in fl:
@@ -33,34 +43,40 @@ async def doit(chat, match):
             await chat.reply('unknown flag: {}'.format(f))
             return
 
-    if 'reply_to_message' in chat.message and 'text' in chat.message['reply_to_message']:
+    # Handle replies
+    if 'reply_to_message' in chat.message:
+        # Try to find the original message text
+        message = chat.message['reply_to_message']
+        original = find_original(message)
+        if not original:
+            return
+
+        # Substitute the text
         try:
-            s, i = re.subn(fr, to, chat.message['reply_to_message']['text'])
+            s, i = re.subn(fr, to, original)
             if i > 0:
-                return (await Chat.from_message(bot, chat.message['reply_to_message']).reply(s))['result']
+                return (await Chat.from_message(bot, message).reply(s))['result']
         except Exception as e:
             await chat.reply('u dun goofed m8: ' + str(e))
             return
-    else:
-        global last_msgs
-        if chat.id not in last_msgs:
+
+    # Try matching the last few messages
+    global last_msgs
+    if chat.id not in last_msgs:
+        return
+
+    for msg in reversed(last_msgs[chat.id]):
+        try:
+            original = find_original(msg)
+            if not original:
+                continue
+
+            s, i = re.subn(fr, to, original, count=count, flags=flags)
+            if i > 0:
+                return (await Chat.from_message(bot, msg).reply(s))['result']
+        except Exception as e:
+            await chat.reply('u dun goofed m8: ' + str(e))
             return
-
-        for msg in reversed(last_msgs[chat.id]):
-            try:
-                if 'text' in msg:
-                    original = msg['text']
-                elif 'caption' in msg:
-                    original = msg['caption']
-                else:
-                    continue
-
-                s, i = re.subn(fr, to, original, count=count, flags=flags)
-                if i > 0:
-                    return (await Chat.from_message(bot, msg).reply(s))['result']
-            except Exception as e:
-                await chat.reply('u dun goofed m8: ' + str(e))
-                return
 
 
 @bot.command(r'^s/((?:\\/|[^/])+)/((?:\\/|[^/])*)(/.*)?')
@@ -74,6 +90,7 @@ async def test(chat, match):
 
 
 @bot.command(r'(.*)')
+@bot.handle('photo')
 async def msg(chat, match):
     global last_msgs
 
@@ -83,19 +100,6 @@ async def msg(chat, match):
     last_msgs[chat.id].append(chat.message)
     if len(last_msgs[chat.id]) > 10:
         last_msgs[chat.id] = last_msgs[chat.id][-10:]
-
-
-@bot.handle('photo')
-async def msg_photo(chat, match):
-    global last_msgs
-
-    if chat.id not in last_msgs:
-        last_msgs[chat.id] = []
-
-    last_msgs[chat.id].append(chat.message)
-    if len(last_msgs[chat.id]) > 10:
-        last_msgs[chat.id] = last_msgs[chat.id][-10:]
-
 
 
 async def main():
